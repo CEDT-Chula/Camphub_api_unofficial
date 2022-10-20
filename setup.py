344b79a6030e396ac2552d3b2320dd3e611be1f2
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-10-21 04:17:09 +0700

    update ignore & fix dist setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ long_description = open(this_directory / 'README.md', encoding='utf-8').read()
 
 setup(
     name='camp_parser',
+    packages=find_packages(),
     description="""unofficial api to get information from camphub and other camp/competition news website. in thai""",
     long_description_content_type='text/markdown',
     long_description=long_description,
@@ -14,8 +15,6 @@ setup(
     license='MIT',
     author="HRNPH",
     author_email='hirunkul2548@gmail.com',
-    packages=find_packages('camp_parser'),
-    package_dir={'': 'camp_parser'},
     url='https://github.com/HRNPH/Camphub_api_unofficial',
     keywords='camp_parser',
     install_requires=[
