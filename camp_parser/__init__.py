b1ff119a5f891b2ed2e71e64810b39720be6dd8e
HRNPH <51855316+HRNPH@users.noreply.github.com>
2022-10-21 03:32:28 +0700

    structure the files for packaging

---
+++ camp_parser/__init__.py
@@ -0,0 +1,4 @@
+import requests
+import bs4 as bs
+import re
+from camp_parser.camp_parser import camphub_parser
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='camp_parser',
+    version='0.1',
+    license='MIT',
+    author="HRNPH",
+    author_email='hirunkul2548@gmail.com',
+    packages=find_packages('camp_parser'),
+    package_dir={'': 'camp_parser'},
+    url='https://github.com/HRNPH/Camphub_api_unofficial',
+    keywords='camp_parser',
+    install_requires=[
+          'requests',
+            'bs4',
+            're',
+      ],
+
+)
